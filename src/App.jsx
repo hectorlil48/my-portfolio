2fb49cd3450dd0ee2959703c8e32246feadb0b1e
Hector Ramirez <67031386+hectorlil48@users.noreply.github.com>
2025-05-20 21:12:17 -0500

    Added custom variant for dark mode on tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,9 @@ import Hero from "./components/Hero";
 
 const App = () => {
   return (
-    <>
+    <div className="min-h-screen bg-white text-gray-900">
       <Navbar />
-      <Hero />
-    </>
+    </div>
   );
 };
 
