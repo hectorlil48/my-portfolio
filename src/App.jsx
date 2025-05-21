7638e91bb8d5941ff604b52f46bcdba6b7337f87
Hector Ramirez <67031386+hectorlil48@users.noreply.github.com>
2025-05-20 20:46:15 -0500

    Created a components folder with a Navbar and Hero component and added them to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,12 @@
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+
 const App = () => {
   return (
-    <div>
-      <h1 className="text-3xl font-bold underline">Portfolio</h1>
-    </div>
+    <>
+      <Navbar />
+      <Hero />
+    </>
   );
 };
 
