8cc92e82807d0dfa7140431d7585ea5c718ea6d3
Hector Ramirez <67031386+hectorlil48@users.noreply.github.com>
2025-05-21 19:25:19 -0500

    Added a primary text color and a bg-color to custom theme colors. Added styles to logo and have a placement svg.

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Hero from "./components/Hero";
 
 const App = () => {
   return (
-    <div className="min-h-screen bg-gray-100">
+    <div className="min-h-screen bg-bg-background">
       <Navbar />
     </div>
   );
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,27 @@
 const Navbar = () => {
   return (
     <nav className="bg-transparent fixed top-0 w-full z-50 transition-all duration-300 py-4">
-      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 flex items-center justify-between">
-        <div className="border border-sky-400">
-          <h1>Ramirez.</h1>
+      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 flex items-center justify-between h-14">
+        <div>
+          <h1 className="text-xl font-bold text-primary">
+            <a href="/">Ramirez.</a>
+          </h1>
         </div>
         <div className="border border-amber-300">
           <h1>Navbar</h1>
+          <svg
+            class="w-6 h-6"
+            fill="none"
+            stroke="currentColor"
+            stroke-width="2"
+            viewBox="0 0 24 24"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M4 6h16M4 12h16M4 18h16"
+            />
+          </svg>
         </div>
       </div>
     </nav>
