e192f5295d65b2569f85ef8d270bd94158a0302c
Hector Ramirez <67031386+hectorlil48@users.noreply.github.com>
2025-05-21 20:37:11 -0500

    Installed heroicons on react. Import a hamburger menu icon and a sun icon for my navigation.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,27 +1,27 @@
+import { Bars3Icon } from "@heroicons/react/24/outline";
+import { SunIcon } from "@heroicons/react/24/outline";
+
 const Navbar = () => {
   return (
     <nav className="bg-transparent fixed top-0 w-full z-50 transition-all duration-300 py-4">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 flex items-center justify-between h-14">
         <div>
-          <h1 className="text-xl font-bold text-primary">
-            <a href="/">Ramirez.</a>
+          <h1 className="text-xl font-bold text-primary ">
+            <a
+              href="/"
+              className="focus:outline-none focus-visible:ring-2 focus-visible:ring-primary"
+            >
+              Ramirez.
+            </a>
           </h1>
         </div>
-        <div className="border border-amber-300">
-          <h1>Navbar</h1>
-          <svg
-            class="w-6 h-6"
-            fill="none"
-            stroke="currentColor"
-            stroke-width="2"
-            viewBox="0 0 24 24"
-          >
-            <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              d="M4 6h16M4 12h16M4 18h16"
-            />
-          </svg>
+        <div className="flex items-center justify-center gap-4">
+          <button>
+            <SunIcon className="h-5 w-5" />
+          </button>
+          <button className="text-gray-500 hover:text-primary focus:outline-none focus-visible:ring-2 focus-visible:ring-primary">
+            <Bars3Icon className="h-6 w-6" />
+          </button>
         </div>
       </div>
     </nav>
