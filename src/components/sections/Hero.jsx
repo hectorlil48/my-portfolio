d4c1bd3e873297ac7cada5d1517398980e7c64c4
Hector Ramirez <67031386+hectorlil48@users.noreply.github.com>
2025-05-26 10:39:35 -0500

    Added header and paragraph to the hero section.

--- src/components/sections/Hero.jsx
+++ src/components/sections/Hero.jsx
@@ -2,27 +2,39 @@ import TypewriterComponent from "typewriter-effect";
 
 const Hero = () => {
   return (
-    <section id="home" className="min-h-screen border-2 border-amber-400">
-      <div>
-        <h1 className="text-heading">
-          Hi, I'm <span className="text-primary">Hector</span>
+    <section
+      id="home"
+      className="font-inter mx-auto flex min-h-screen max-w-7xl flex-col gap-10 px-4 py-24"
+    >
+      <div className="flex flex-col gap-5 text-center">
+        <span className="text-primary text-sm font-medium transition-[font-size] duration-300 md:text-base">
+          Hello, I'm
+        </span>
+        <h1 className="text-heading text-4xl font-bold transition-[font-size] duration-300 md:text-5xl lg:text-6xl">
+          Hector <span className="text-primary">Ramirez</span>
         </h1>
 
-        <div className="mb-4 h-10 text-2xl font-medium sm:text-3xl">
+        <div className="text-foreground text-2xl font-medium md:text-2xl">
           <TypewriterComponent
             options={{
               strings: [
                 "Full-Stack Developer",
                 "React & Django Enthusiast",
                 "Builder of Modern Web Apps",
+                "Always Learning, Always Building",
               ],
               autoStart: true,
               loop: true,
             }}
           />
         </div>
+
+        <p className="text-foreground">
+          I build responsive, full-stack web apps with React, Node.js, and
+          Django. Focused on clean code, modern UI, and solving real problems.
+        </p>
       </div>
-      <div></div>
+      <div className="bg-primary h-[278px] rounded-2xl"></div>
     </section>
   );
 };
