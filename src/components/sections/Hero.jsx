c5c52e83780d9317c6594cfe1fd5165a1f873b73
Hector Ramirez <67031386+hectorlil48@users.noreply.github.com>
2025-05-26 09:15:52 -0500

    Added 2 divs to Hero section, was messing with TypeWriterComponent on react.

--- src/components/sections/Hero.jsx
+++ src/components/sections/Hero.jsx
@@ -2,11 +2,28 @@ import TypewriterComponent from "typewriter-effect";
 
 const Hero = () => {
   return (
-    <div id="home" className="min-h-screen border-2 border-amber-400">
-      <div className="mx-auto max-w-7xl border bg-sky-400">
-        <h1>Hello World</h1>
+    <section id="home" className="min-h-screen border-2 border-amber-400">
+      <div>
+        <h1 className="text-heading">
+          Hi, I'm <span className="text-primary">Hector</span>
+        </h1>
+
+        <div className="mb-4 h-10 text-2xl font-medium sm:text-3xl">
+          <TypewriterComponent
+            options={{
+              strings: [
+                "Full-Stack Developer",
+                "React & Django Enthusiast",
+                "Builder of Modern Web Apps",
+              ],
+              autoStart: true,
+              loop: true,
+            }}
+          />
+        </div>
       </div>
-    </div>
+      <div></div>
+    </section>
   );
 };
 
