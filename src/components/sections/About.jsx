aaabb79eb130aece297d6f86416f0b5ced668406
Hector Ramirez <67031386+hectorlil48@users.noreply.github.com>
2025-06-02 20:44:07 -0500

    Changed padding to Hero and about sections.

--- src/components/sections/About.jsx
+++ src/components/sections/About.jsx
@@ -2,8 +2,8 @@ import React from "react";
 
 const About = () => {
   return (
-    <section id="about">
-      <header className="mx-auto max-w-7xl px-4">
+    <section id="about" className="mx-auto max-w-7xl px-4 lg:px-8">
+      <header>
         <div>
           <h2 className="text-heading text-4xl font-bold">About Me</h2>
           <div className="bg-primary h-[5px] w-14 rounded"></div>
