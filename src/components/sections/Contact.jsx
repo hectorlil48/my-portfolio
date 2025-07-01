8c9d3bc6548887e1d53e5e5de9984ff8014edad9
Hector Ramirez <67031386+hectorlil48@users.noreply.github.com>
2025-07-01 16:51:34 -0500

    Added socials to contact section.

--- src/components/sections/Contact.jsx
+++ src/components/sections/Contact.jsx
@@ -1,3 +1,5 @@
+import { Mail, Phone, MapPin, Github } from "lucide-react";
+
 const Contact = () => {
   return (
     <section id="contact" className="mx-auto max-w-6xl px-6 py-20">
@@ -26,8 +28,50 @@ const Contact = () => {
             </p>
           </div>
 
-          <div className="space-y-4">
-            <div></div>
+          <div className="mb-6 space-y-4">
+            <div className="flex gap-3">
+              <Mail className="text-primary mt-1 h-5 w-5" />
+              <div>
+                <h4 className="text-foreground font-medium">Email</h4>
+                <p className="text-muted text-sm">
+                  <a href="mailto:hramir94@gmail.com">hramir94@gmail.com</a>
+                </p>
+              </div>
+            </div>
+
+            <div className="flex gap-3">
+              <Phone className="text-primary mt-1 h-5 w-5" />
+              <div>
+                <h4 className="text-foreground font-medium">Phone</h4>
+                <p className="text-muted text-sm">
+                  <a href="tel:7792790100">(779)-279-0100</a>
+                </p>
+              </div>
+            </div>
+
+            <div className="flex gap-3">
+              <MapPin className="text-primary mt-1 h-5 w-5" />
+              <div>
+                <h4 className="text-foreground font-medium">Location</h4>
+                <p className="text-muted text-sm">Chicago, IL</p>
+              </div>
+            </div>
+          </div>
+
+          <div>
+            <h3 className="text-heading mb-3 text-lg font-semibold">
+              Connect with me
+            </h3>
+            <div>
+              <a
+                href=""
+                target="_blank"
+                rel="noopener noreferrer"
+                aria-label="GitHub"
+              >
+                <Github />
+              </a>
+            </div>
           </div>
         </div>
         <div></div>
