5f2de783bd5603ea8d0f94d0fdf46ac2d3e893e8
Hector Ramirez <67031386+hectorlil48@users.noreply.github.com>
2025-07-13 15:09:43 -0500

    fix: Changed the footer to include deployed on vercel

--- src/components/sections/Footer.jsx
+++ src/components/sections/Footer.jsx
@@ -1,13 +1,13 @@
 const Footer = () => {
   return (
     <footer className="bg-background border-skill-border text-foreground border-t px-6 py-10 transition-colors duration-300">
-      <div className="mx-auto flex max-w-6xl flex-col items-center justify-between gap-4 md:flex-row">
+      <div className="mx-auto flex max-w-6xl flex-col items-center justify-between gap-2 md:flex-row">
         <p className="text-sm">
           &copy; {new Date().getFullYear()} Hector Ramirez. All rights reserved.
         </p>
 
-        <p className="text-muted text-sm">
-          Built with{" "}
+        <p className="text-muted text-center text-sm md:text-right">
+          Designed & built with{" "}
           <a
             href="https://reactjs.org"
             target="_blank"
@@ -15,8 +15,8 @@ const Footer = () => {
             className="text-primary font-medium hover:underline"
           >
             React
-          </a>{" "}
-          &{" "}
+          </a>
+          ,{" "}
           <a
             href="https://tailwindcss.com"
             target="_blank"
@@ -25,6 +25,16 @@ const Footer = () => {
           >
             Tailwind CSS
           </a>
+          , deployed on{" "}
+          <a
+            href="https://vercel.com"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="text-primary font-medium hover:underline"
+          >
+            Vercel
+          </a>
+          .
         </p>
       </div>
     </footer>
