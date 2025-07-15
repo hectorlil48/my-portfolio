45f0711a658d055534203dbfe9308641d9504565
Hector Ramirez <67031386+hectorlil48@users.noreply.github.com>
2025-07-14 20:43:45 -0500

    fix: made height 90vh, increased top padding

--- src/components/sections/Contact/Contact.jsx
+++ src/components/sections/Contact/Contact.jsx
@@ -5,7 +5,7 @@ import ContactForm from "./ContactForm";
 
 const Contact = () => {
   return (
-    <section id="contact" className="mx-auto min-h-screen max-w-6xl px-6 py-20">
+    <section id="contact" className="mx-auto min-h-[90vh] max-w-6xl px-6 pt-30">
       <ContactHeader />
 
       <div className="grid grid-cols-1 gap-8 lg:grid-cols-3">
